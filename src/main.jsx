2b8e007c7f228ba9493e7a62af113c40dfe7ddf8
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-09-01 07:16:11 +0800

    Load the typing-first Wordspace redesign

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './wordspace-v6.css'
+import './wordspace-v8.css'
 
 createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>)
