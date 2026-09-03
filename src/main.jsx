9c4cdc39a71f573a1e87bdab73d8fc163f838c9d
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-09-04 05:45:15 +0800

    Load redesigned settings styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './wordspace-v8.css'
 import './soundpacks.css'
+import './settings-v2.css'
 
 createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>)
